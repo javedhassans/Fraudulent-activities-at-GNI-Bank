2f20d6adf73234aebed14aaf09dd190b1ed542a2
Javed Hassan Sabu <50239488+javedhassans@users.noreply.github.com>
2020-02-14 22:28:20 +0100

    initial Files
    This is the assignment for the class

---
+++ assaignment01.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pandas as pd
+#%%
+df = pd.read_csv(Path().joinpath('transactions.csv'))
+#%%
+# path = '/home/ubuntu/Downloads/Data_analytics/assaignment_01/transactions.csv'
+# df = pd.read_csv(path)
+# df.head(10)
+#%%
+df.info()
+
+#%%
+df.head(10)
+#%%
+df.isnull().sum()
+#%%
+
