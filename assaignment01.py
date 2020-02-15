4f48adafbf0fd1f25b62fe60597a0bf00f4765ae
javedhassans <javedhassans@outlook.com>
2020-02-15 17:03:23 +0100

    it is the demo

--- assaignment01.py
+++ assaignment01.py
@@ -1,18 +1,19 @@
 from pathlib import Path
-
 import pandas as pd
 #%%
+# fancy way to load data
 df = pd.read_csv(Path().joinpath('transactions.csv'))
 #%%
+# another way to load data
 # path = '/home/ubuntu/Downloads/Data_analytics/assaignment_01/transactions.csv'
 # df = pd.read_csv(path)
 # df.head(10)
 #%%
+# to look into the data
 df.info()
-
 #%%
 df.head(10)
 #%%
 df.isnull().sum()
 #%%
-print('hey') #whatsup
+df.tail(10)
